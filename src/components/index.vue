b791d7b8fa217da45adbe05fb4d638f86aab845c
侯春智 <houcz@zqnb.com.cn>
2020-09-17 11:35:04 +0800

    houcz9个人简历

---
+++ src/components/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="index">
+		<div class="f-full content">
+			<div class="corner-text">UI/UX Design</div>
+			<div class="main-text">
+				<p>HOUCZ9-</p>
+				<p>DESIGN WORKS <strong>作品集</strong></p>
+				<div class="author-wrap">
+					<p class="text">侯春智  设计</p>
+					<div class="website flex">
+						<a href="https://www.zcool.com.cn/u/668029" target="_blank"  title="站酷上的作品"><i class="icon-zcool"></i></a>
+						<a href="https://i.ui.cn/ucenter/891224.html" target="_blank"  title="UI中国上的作品"><i class="icon-ui"></i></a>
+						<a href="https://dribbble.com/houcz9" target="_blank"  title="Dribbble上的作品"><i class="icon-dribbble"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="corner-mouse"></div>
+			<div class="corner-right"></div>
+			
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'index',
+		data() {
+			return {
+				
+			}
+		},
+		created() {
+		},
+		mounted(){
+			
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
